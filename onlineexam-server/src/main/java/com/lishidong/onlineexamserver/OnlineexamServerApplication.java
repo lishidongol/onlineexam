990ef0bc1ab684a054bd5d5d4b1d3224115a40b3
lishidongol <996479960@qq.com>
2022-06-21 14:38:17 +0800

    在线考试系统初始化

---
+++ onlineexam-server/src/main/java/com/lishidong/onlineexamserver/OnlineexamServerApplication.java
@@ -0,0 +1,13 @@
+package com.lishidong.onlineexamserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineexamServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineexamServerApplication.class, args);
+    }
+
+}
---
+++ onlineexam-server/src/test/java/com/lishidong/onlineexamserver/OnlineexamServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lishidong.onlineexamserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineexamServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
